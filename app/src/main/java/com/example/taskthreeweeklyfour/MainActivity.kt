793bd4237ab1360b47a938e3b5e2ce2ef681c1c4
Vlad232008 <vlad2320082009@gmail.com>
2022-05-25 21:06:32 +0400

    Программная прорисовка

--- app/src/main/java/com/example/taskthreeweeklyfour/MainActivity.kt
+++ app/src/main/java/com/example/taskthreeweeklyfour/MainActivity.kt
@@ -5,10 +5,14 @@ import android.graphics.Typeface
 import android.os.Bundle
 import android.view.Gravity
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.view.marginEnd
+import com.bumptech.glide.Glide
 import com.github.javafaker.Faker
 
 class MainActivity : AppCompatActivity() {
@@ -20,36 +24,59 @@ class MainActivity : AppCompatActivity() {
         ChtoEtoTakoe()
     }
 
-    private fun ChtoEtoTakoe(){
+    private fun ChtoEtoTakoe() {
+        val linearLayoutHor = LinearLayout(this)
+        linearLayoutHor.orientation = LinearLayout.VERTICAL
 
-        val linearLayout = LinearLayout(this)
-        linearLayout.orientation = LinearLayout.VERTICAL
+        val layoutManager: LinearLayout.LayoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        layoutManager.setMargins(10, 10, 10, 0)
+        layoutManager.weight = 1F
+        val linearlayoutManager: LinearLayout.LayoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        linearlayoutManager.setMargins(10, 0, 10, 10)
+        linearlayoutManager.weight = 1F
         val scrollView: ScrollView = findViewById(R.id.scrollView)
         for (i in 1..100) {
+            val cardView = CardView(this)
+            cardView.radius = 15f
+            cardView.setCardBackgroundColor(Color.parseColor("#FF7145"))
+            cardView.setContentPadding(10,10,10,10)
+            cardView.layoutParams = layoutManager
+            cardView.cardElevation = 30f
+            val linearLayout = LinearLayout(this)
+            linearLayout.orientation = LinearLayout.VERTICAL
+            /*val image = ImageView(this)*/
             val username = TextView(this)
             val message = TextView(this)
-            if (username.parent != null) {
-                (username.parent as ViewGroup).removeView(username)
-            }
-            if (message.parent != null) {
-                (message.parent as ViewGroup).removeView(message)
-            }
             username.text = faker.name().name()
+            username.layoutParams = layoutManager
             username.typeface = Typeface.SERIF
-            username.gravity = Gravity.CENTER
-            username.textSize = 24F
+            username.gravity = Gravity.START
+            username.textSize = 16F
+            username.setPadding(5, 5, 5, 5)
             username.setTextColor(Color.BLACK)
-            username.setBackgroundColor(Color.BLUE)
+            username.setBackgroundColor(Color.parseColor("#FF7145"))
 
             message.text = faker.company().name()
+
             message.typeface = Typeface.SERIF
-            message.gravity = Gravity.CENTER
-            message.textSize = 24F
-            message.setTextColor(Color.BLACK)
-            message.setBackgroundColor(Color.BLUE)
+            message.gravity = Gravity.START
+            message.textSize = 12F
+            message.setTextColor(Color.GRAY)
+            message.setBackgroundColor(Color.parseColor("#FF7145"))
+            message.layoutParams = linearlayoutManager
+            message.setPadding(5, 5, 5, 5)
             linearLayout.addView(username)
             linearLayout.addView(message)
+            cardView.addView(linearLayout)
+            linearLayoutHor.addView(cardView)
+            //linearLayoutHor.addView(linearLayout)
         }
-        scrollView.addView(linearLayout)
+        scrollView.addView(linearLayoutHor)
     }
 }
